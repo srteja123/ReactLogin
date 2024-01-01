20675d495896deabfb799856bcee844f3eff85ad
srteja123 <39849488+srteja123@users.noreply.github.com>
2024-01-01 17:13:35 +0530

    Update authenticationService.js

--- client/src/api/authenticationService.js
+++ client/src/api/authenticationService.js
@@ -20,13 +20,45 @@ export const userLogin=(authRequest)=>{
         'data':authRequest
     })
 }
-
+//Setting bearer token for dashboard screen
 export const fetchUserData=()=>{
     return axios({
         method:'GET',
-        url:`${process.env.hostUrl||'http://localhost:8080'}/dashboard/view`,
+        url:`${process.env.hostUrl||'http://localhost:8080'}/user/dashboard`,
         headers:{
             'Authorization':'Bearer '+getToken()
         }
+    })
+}
+//fetch all data for admin
+export const fetchAllUsersData=()=>{
+        return axios({
+            method:'GET',
+            url:`${process.env.hostUrl||'http://localhost:8080'}/user/admin/dashboard`,
+            headers:{
+                'Authorization':'Bearer '+getToken()
+            }
+        })
+}
+
+export const deleteUser=(authRequest)=>{
+    return axios({
+        method:'POST',
+        url:`${process.env.hostUrl||'http://localhost:8080'}/user/admin/deleteUser`,
+        headers:{
+            'Authorization':'Bearer '+getToken()
+        },
+        'data':authRequest
+    })
+}
+
+export const updateUser=(authRequest)=>{
+    return axios({
+        method:'POST',
+        url:`${process.env.hostUrl||'http://localhost:8080'}/user/admin/editUser`,
+        headers:{
+            'Authorization':'Bearer '+getToken()
+        },
+        'data':authRequest
     })
 }
