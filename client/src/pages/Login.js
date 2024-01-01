e87de23d166a464766a001f461fe641c1d15574a
srteja123 <39849488+srteja123@users.noreply.github.com>
2024-01-01 17:18:10 +0530

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from 'react';
-import { connect } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { Link, useNavigate } from "react-router-dom";
-import Cookies from 'universal-cookie';
+import validator from 'validator';
 import { userLogin } from '../api/authenticationService';
-import { authFailure, authSuccess, authenticate } from '../redux/authActions';
 import './../App.css';
-
 
 
 const LoginPage = ({ loading, error, ...props }) =>{
@@ -14,45 +11,53 @@ const LoginPage = ({ loading, error, ...props }) =>{
         emailId: '',
         password: '',
     });
-    
+    const [errorEmail, setEmailError] = useState({
+        error: true,
+        success: false
+    });
+    const [errorResponse, setErrorResponse] = useState({
+        error: false,
+        message:"",
+    });
+    useEffect(()=>{
+        //Code to be called on load
+        setEmailError({ error: false, success: true });
+       // setErrorResponse({error: false, message :''});
+        }, []);
     const navigate=useNavigate();
     const handleSubmit = (evt) => {
-        console.log("Clicked");
         evt.preventDefault();
-        props.authenticate();
 
         userLogin(values).then((response) => {
-
-            console.log("response", response);
             if (response.status === 200) {
-                props.setUser(response.data);
-                const cookies = new Cookies();
-                cookies.set('myToken', response.data.accessToken);
-                navigate('/dashboard');
-                
+                //Setting token in local storage
+                localStorage.setItem('USER_KEY',response.data.token);
+                const isAdmin = response.data.roles.includes('ADMIN');
+                if(isAdmin){
+                    navigate('/adminDashboard');
+                }else{
+                    navigate('/dashboard');
+                }
+            
             }
             else {
-                props.loginFailure('Something Wrong!Please Try Again Login');
+                setErrorResponse({error: true, message :response.message});
             }
-
-
         }).catch((err) => {
-            console.log(" Error " + err);
             if (err && err.response) {
-
                 switch (err.response.status) {
+                    case 403:
+                        setErrorResponse({error: true, message :err.response.data.message});
+                        break;
                     case 401:
-                        console.log("401 status");
-                        props.loginFailure("Authentication Failed.Bad Credentials");
+                        setErrorResponse({error: true, message :err.response.data.message});
                         break;
                     default:
-                        props.loginFailure('Something Wrong!Please Try Again error');
-
+                        setErrorResponse({error: true, message :err.response.data.message});
                 }
-
             }
             else {
-                props.loginFailure('Something Wrong!Please Try Again else');
+                setErrorResponse({error: true, message :'Failed Login'});
             }
 
         });
@@ -65,6 +70,15 @@ const LoginPage = ({ loading, error, ...props }) =>{
             ...values,
             [e.target.name]: e.target.value
         }));
+        if (e.target.name === 'emailId') {
+            var email = e.target.value;
+
+            if (!validator.isEmail(email)) {
+                setEmailError({ error: true, success: false });
+            }else{
+                setEmailError({ error: false, success: true });
+            }
+        }
     };
         return(
             <div className="signup-form">
@@ -76,10 +90,11 @@ const LoginPage = ({ loading, error, ...props }) =>{
                     <div className="form-group">
                         <input type="password" value={values.password} onChange={handleChange} className="form-control" name="password" placeholder="Password" required="required" />
                     </div>
-                  
                     <div className="form-group">
-                        <button type="submit" className="btn btn-success btn-lg btn-block">Login</button>
+                        <button type="submit" data-testid="login" className="btn btn-success btn-lg btn-block">Login</button>
                     </div>
+                    <p className="text-error">{errorEmail.error ? "Please enter a valid email format" : null}</p>
+                    <p className="text-error">{errorResponse.error ? errorResponse.message : null}</p>
                 </form>
                 <div className="text-center">Don't have an account? <Link to="/register">Sign Up</Link></div>
             </div>
@@ -88,22 +103,4 @@ const LoginPage = ({ loading, error, ...props }) =>{
     
 }
 
-const mapStateToProps=({auth})=>{
-    console.log("login state ",auth);
-    return {
-        loading:auth.loading,
-        error:auth.error
-}}
-
-
-const mapDispatchToProps=(dispatch)=>{
-    console.log("map Dispatch to props ");
-    return {
-        authenticate :()=> dispatch(authenticate()),
-        setUser:(data)=> dispatch(authSuccess(data)),
-        loginFailure:(message)=>dispatch(authFailure(message))
-    }
-}
-
-
-export default connect(mapStateToProps,mapDispatchToProps)(LoginPage);
+export default LoginPage;
