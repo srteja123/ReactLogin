cb7f323ff3df10e88828f42b979d03396064a42d
srteja123 <39849488+srteja123@users.noreply.github.com>
2024-01-01 17:21:59 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PublicRoutes from "./api/publicRoutes";
+import AddUserModal from "./pages/AddUser";
+import AdminDashBoard from "./pages/AdminDashBoard";
 import Dashboard from "./pages/Dashboard";
 import LoginPage from "./pages/Login";
 import RegisterPage from "./pages/Reg";
 
+
+//Below code is used for routing purpose for login,register and dashboard
+//Public Routes is used to allow users to navigate to dashboard once they have the token
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-          
         <Route path="/" element={<PublicRoutes/>}>
           <Route path="/" element={<LoginPage/>}/>
         </Route>
-          <Route path="/register" element={<RegisterPage />}>         
+          <Route path="/register" element={<RegisterPage />}>
           </Route>
-          <Route path="/dashboard/*" element={<Dashboard />}>         
+          <Route path="/dashboard" element={<Dashboard />}>
+          </Route>
+          <Route path="/adminDashboard" element={<AdminDashBoard />}>
+          </Route>
+          <Route path="/AddUserModal" element={<AddUserModal />}>
           </Route>
         </Routes>
       </BrowserRouter>
